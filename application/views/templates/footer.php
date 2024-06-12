f3dae5422beb9c2fce17f07826145fb47cd2baf0
Basile Legrelle <basilelegrelle@gmail.com>
2024-06-12 15:27:43 +0200

    Fixe footer et navbar

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,7 @@
-<footer class="bg-light text-center text-lg-start mt-auto">
-	<div class="container p-4">
+</div> <!-- Fin du conteneur principal -->
+
+<footer class="bg-light text-center text-lg-start mt-auto w-100">
+	<div class="container-fluid p-4">
 		<div class="row">
 			<div class="col-lg-6 col-md-12 mb-4 mb-md-0">
 				<h5 class="text-uppercase">À propos de TuneTap</h5>
@@ -12,16 +14,16 @@
 				<h5 class="text-uppercase">Liens utiles</h5>
 				<ul class="list-unstyled mb-0">
 					<li>
-						<a href="#!" class="text-dark">Accueil</a>
+						<a href="<?php echo site_url('home'); ?>" class="text-dark">Accueil</a>
 					</li>
 					<li>
-						<a href="#!" class="text-dark">Albums</a>
+						<a href="<?php echo site_url('albums'); ?>" class="text-dark">Albums</a>
 					</li>
 					<li>
-						<a href="#!" class="text-dark">Artistes</a>
+						<a href="<?php echo site_url('artists'); ?>" class="text-dark">Artistes</a>
 					</li>
 					<li>
-						<a href="#!" class="text-dark">Chansons</a>
+						<a href="<?php echo site_url('songs'); ?>" class="text-dark">Chansons</a>
 					</li>
 				</ul>
 			</div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,53 +4,65 @@
 	<title><?php echo $title; ?></title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 	<style>
-		body {
+		body, html {
+			height: 100%;
+			margin: 0;
+			display: flex;
+			flex-direction: column;
+		}
+		.content {
+			flex: 1 0 auto;
 			padding-top: 70px; /* Ajustez cette valeur en fonction de la hauteur de votre navbar */
+		}
+		footer {
+			flex-shrink: 0;
 		}
 	</style>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-	<a class="navbar-brand" href="<?php echo site_url('home'); ?>">TuneTap</a>
-	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-		<span class="navbar-toggler-icon"></span>
-	</button>
-	<div class="collapse navbar-collapse" id="navbarNav">
-		<ul class="navbar-nav mr-auto">
-			<li class="nav-item">
-				<a class="nav-link" href="<?php echo site_url('home'); ?>">Accueil</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="<?php echo site_url('albums'); ?>">Albums</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="<?php echo site_url('artists'); ?>">Artistes</a>
-			</li>
-			<li class="nav-item">
-				<a class="nav-link" href="<?php echo site_url('songs'); ?>">Chansons</a>
-			</li>
-		</ul>
-		<form class="form-inline my-2 my-lg-0" action="<?php echo site_url('search'); ?>" method="get">
-			<input class="form-control mr-sm-2" type="search" placeholder="Rechercher" aria-label="Search" name="q">
-			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
-		</form>
-		<ul class="navbar-nav ml-auto">
-			<?php if ($this->session->userdata('logged_in')): ?>
+	<div class="container-fluid">
+		<a class="navbar-brand" href="<?php echo site_url('home'); ?>">TuneTap</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+		<div class="collapse navbar-collapse" id="navbarNav">
+			<ul class="navbar-nav mr-auto">
 				<li class="nav-item">
-					<a class="nav-link" href="<?php echo site_url('dashboard'); ?>"><?php echo $this->session->userdata('username'); ?></a>
+					<a class="nav-link" href="<?php echo site_url('home'); ?>">Accueil</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="<?php echo site_url('login/logout'); ?>">Se déconnecter</a>
-				</li>
-			<?php else: ?>
-				<li class="nav-item">
-					<a class="nav-link" href="<?php echo site_url('login'); ?>">Se connecter</a>
+					<a class="nav-link" href="<?php echo site_url('albums'); ?>">Albums</a>
 				</li>
 				<li class="nav-item">
-					<a class="nav-link" href="<?php echo site_url('register'); ?>">S'inscrire</a>
+					<a class="nav-link" href="<?php echo site_url('artists'); ?>">Artistes</a>
 				</li>
-			<?php endif; ?>
-		</ul>
+				<li class="nav-item">
+					<a class="nav-link" href="<?php echo site_url('songs'); ?>">Chansons</a>
+				</li>
+			</ul>
+			<form class="form-inline my-2 my-lg-0" action="<?php echo site_url('search'); ?>" method="get">
+				<input class="form-control mr-sm-2" type="search" placeholder="Rechercher" aria-label="Search" name="q">
+				<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Rechercher</button>
+			</form>
+			<ul class="navbar-nav ml-auto">
+				<?php if ($this->session->userdata('logged_in')): ?>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo site_url('dashboard'); ?>"><?php echo $this->session->userdata('username'); ?></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo site_url('login/logout'); ?>">Se déconnecter</a>
+					</li>
+				<?php else: ?>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo site_url('login'); ?>">Se connecter</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="<?php echo site_url('register'); ?>">S'inscrire</a>
+					</li>
+				<?php endif; ?>
+			</ul>
+		</div>
 	</div>
 </nav>
-<div class="container">
+<div class="container content">
